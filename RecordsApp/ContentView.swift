6b5d63ba63afa3f24bee1dec5bb374216628e0bf
aryaIOS <arya.vashisht@eka.care>
2025-01-22 15:47:33 +0530

    Initial Commit

---
+++ RecordsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecordsApp
+//
+//  Created by Arya Vashisht on 22/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ RecordsApp/RecordsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordsAppApp.swift
+//  RecordsApp
+//
+//  Created by Arya Vashisht on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct RecordsAppApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}

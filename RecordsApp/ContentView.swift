e4eeb8f36f92b286456c069130d6a361bb1d7727
aryaIOS <arya.vashisht@eka.care>
2025-01-23 20:26:21 +0530

    Syncing record completed

--- RecordsApp/ContentView.swift
+++ RecordsApp/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import EkaMedicalRecordsUI
+import EkaMedicalRecordsCore
 
 struct ContentView: View {
+  // MARK: - Properties
+  
+  let recordsRepo = RecordsRepo()
+  
+  // MARK: - Body
+  
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    NavigationView { // Add NavigationView here
+      VStack {
+        Image(systemName: "globe")
+          .imageScale(.large)
+          .foregroundStyle(.tint)
+        Text("Hello, world!")
+        
+        NavigationLink(
+          destination: RecordsGridListView().environment(\.managedObjectContext, recordsRepo.databaseManager.container.viewContext)
+        ) {
+          Text("Go to Records")
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(8)
+        }
+      }
+      .padding()
     }
-    .padding()
   }
 }
 
--- RecordsApp/RecordsAppApp.swift
+++ RecordsApp/RecordsAppApp.swift
@@ -6,12 +6,55 @@
 //
 
 import SwiftUI
+import EkaMedicalRecordsUI
+import EkaMedicalRecordsCore
 
 @main
 struct RecordsAppApp: App {
+  
+  // MARK: - Properties
+  
+  let recordsRepo = RecordsRepo()
+  
+  // MARK: - Init
+  
+  init() {
+    registerUISdk()
+    registerCoreSdk()
+  }
+  
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
+  }
+}
+
+// MARK: - UI SDK Init
+
+extension RecordsAppApp {
+  func registerUISdk() {
+    registerFonts()
+  }
+  
+  private func registerFonts() {
+    do {
+      try Fonts.registerAllFonts()
+    } catch {
+      debugPrint("Failed to fetch fonts")
+    }
+  }
+}
+
+// MARK: - Core SDK Init
+
+extension RecordsAppApp {
+  func registerCoreSdk() {
+    registerAuthToken()
+  }
+  
+  private func registerAuthToken() {
+    InitConfigurations.shared.authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZWNiNTJmZDctZGM4Yy00YTg0LWEyZTQtNmZiMThiMDVjOGEwIiwib2lkIjoiMTY3NTM1NDc3MDAxNDI3IiwiZm4iOiJBcnlhIFZhc2hpc2h0IiwiZ2VuIjoiTSIsImlzLXAiOnRydWUsImRvYiI6IjIwMDAtMDQtMDciLCJtb2IiOiIrOTE4KioqKioqNzQ0IiwidHlwZSI6MSwiZG9jLWlkIjoiMTY3NTM1NDc3MDAxNDI3IiwiaXNzIjoiYSIsImF1ZCI6InBhdGllbnQtYXBwLWlvcyIsImlzLWQiOnRydWUsImlhdCI6MTczNzYxNzIzNSwiZXhwIjoxNzM3NjIwODM1fQ.XnhbC-TmMqYi2dEpD71rTlPTOBA2ASVWlC0fWSe7Y2c"
+    InitConfigurations.shared.refreshToken = "7tDsUmiHKN7P6BSY"
   }
 }
